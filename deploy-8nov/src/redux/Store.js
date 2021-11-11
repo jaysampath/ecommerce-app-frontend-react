78a30bf9c977996b47c62c0a08a5ab4a1cda2437
Jay <50858217+jaysampath@users.noreply.github.com>
2021-11-11 16:47:42 +0530

    Delete deploy-8nov directory

--- deploy-8nov/src/redux/Store.js
+++
@@ -1,8 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-import CartSlice from "./CartSlice";
-
-const store = configureStore({
-   reducer:{ cart:CartSlice.reducer}
-});
-
-export default store;
