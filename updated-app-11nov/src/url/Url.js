1ac76668738e27701f311095fb484db871cca375
Jay <50858217+jaysampath@users.noreply.github.com>
2021-11-11 16:50:48 +0530

    Add files via upload

---
+++ updated-app-11nov/src/url/Url.js
@@ -0,0 +1,80 @@
+// export const checkExistingUserLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/user/check/";
+
+// export const loginLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/user/login";
+
+// export const generateOtpLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/otp/generate/";
+
+// export const signupLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/user/signup";
+
+// export const validateOtpLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/otp/validate/";
+
+// export const updatePasswordLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/user/update/";
+
+// export const ItemsBySubCategoriesLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/item/items/";
+
+// export const searchResultsLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/item/search/";
+
+// export const addNewReviewLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/item/review";
+
+// export const itemAllReviewsLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/item/reviews/";
+
+// export const itemDetailLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/item/items/";
+
+// export const allItemsLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/item/items";
+
+// export const itemTopReviewsLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/item/reviews/top/";
+
+// export const saveOrderLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/order/orders";
+
+// export const userOrdersLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/order/orders/";
+
+// export const fetchUserCartLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/cart/";
+
+// export const addItemToCartLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/cart/add/";
+
+// export const addItemQuantityInCartLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/cart/change/add/";
+
+// export const reduceItemQuantityInCartLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/cart/change/reduce/"
+
+// export const deleteItemFromCartLink = "http://ec2-13-234-77-242.ap-south-1.compute.amazonaws.com:8080/cart/delete/";
+
+
+export const checkExistingUserLink = "http://localhost:8080/user/check/";
+
+export const loginLink = "http://localhost:8080/user/login";
+
+export const generateOtpLink = "http://localhost:8080/otp/generate/";
+
+export const signupLink = "http://localhost:8080/user/signup";
+
+export const validateOtpLink = "http://localhost:8080/otp/validate/";
+
+export const updatePasswordLink = "http://localhost:8080/user/update/";
+
+export const ItemsBySubCategoriesLink = "http://localhost:8080/item/items/";
+
+export const searchResultsLink = "http://localhost:8080/item/search/";
+
+export const addNewReviewLink = "http://localhost:8080/item/review";
+
+export const itemAllReviewsLink = "http://localhost:8080/item/reviews/";
+
+export const itemDetailLink = "http://localhost:8080/item/items/";
+
+export const allItemsLink = "http://localhost:8080/item/items";
+
+export const itemTopReviewsLink = "http://localhost:8080/item/reviews/top/";
+
+export const saveOrderLink = "http://localhost:8080/order/orders";
+
+export const userOrdersLink = "http://localhost:8080/order/orders/";
+
+export const fetchUserCartLink = "http://localhost:8080/cart/";
+
+export const addItemToCartLink = "http://localhost:8080/cart/add/";
+
+export const addItemQuantityInCartLink = "http://localhost:8080/cart/change/add/";
+
+export const reduceItemQuantityInCartLink = "http://localhost:8080/cart/change/reduce/"
+
+export const deleteItemFromCartLink = "http://localhost:8080/cart/delete/";
